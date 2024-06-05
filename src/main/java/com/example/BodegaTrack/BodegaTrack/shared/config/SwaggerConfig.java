8e755cb441f1c0a92f63bde55681046ed08dbc72
DarkArcher822 <112427427+nicolaszagal@users.noreply.github.com>
2024-06-05 08:34:01 -0500

    Update SwaggerConfig.java
    Se agrego la clase SecurityConfig para intentar solventar los problemas de autenticacion de swagger

--- src/main/java/com/example/BodegaTrack/BodegaTrack/shared/config/SwaggerConfig.java
+++ src/main/java/com/example/BodegaTrack/BodegaTrack/shared/config/SwaggerConfig.java
@@ -6,6 +6,10 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,5 +20,24 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.example.BodegaTrack.BodegaTrack"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+}
+
+@configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+         http
+             .authorizeRequests()
+             .antMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
+             .anyRequest().authenticated()
+             .and()
+             .formLogin()
+             .and()
+             .logout()
+             .permitAll()
+             .and()
+             .csrf().disable();
     }
 }
